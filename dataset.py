fe8daeecf2f9904e4c96d419055545c6d9d0813a
Sephirothxlx <1216083287@qq.com>
2017-08-14 18:05:27 +0800

    revise extraction.py and dataset.py
    revise get_parents() in extraction.py
    add filter_get_ontology() in dataset.py
    revise get_types() in dataset.py
    add get_subjects() in dataset.py

--- dataset.py
+++ dataset.py
@@ -1,7 +1,11 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import logging
+import pprint
+import re
 
-from SPARQLWrapper import SPARQLWrapper, JSON, XML
+from SPARQLWrapper import SPARQLWrapper, JSON
+
 """
     
 """
@@ -18,18 +22,18 @@ def get_categories(entity_id):
     :return: <list> of entity, each is a category of the target entity
     """
     dbpedia_sql = """
-PREFIX dcterms: <http://purl.org/dc/terms/>
-PREFIX dbo: <http://dbpedia.org/ontology>
-SELECT DISTINCT ?category
-WHERE {
-    {<%s> dcterms:subject ?category .}
-    UNION
-    {<%s> dbo:category ?category .}
-}
-ORDER BY ?category
-    """%(entity_id, entity_id)
-    results = __execute_sparql(DBPEDIA_ENDPOINT, dbpedia_sql)['results']['bindings']
-    return [result['category']['value'] for result in results]
+        PREFIX dcterms: <http://purl.org/dc/terms/>
+        PREFIX dbo: <http://dbpedia.org/ontology>
+        SELECT DISTINCT ?category
+        WHERE {
+            {<%s> dcterms:subject ?category .}
+            UNION
+            {<%s> dbo:category ?category .}
+        }
+        ORDER BY ?category
+    """ % (entity_id, entity_id)
+    results = __execute_sparql(DBPEDIA_ENDPOINT, dbpedia_sql)["results"]["bindings"]
+    return [result["category"]["value"] for result in results]
 
 
 def get_types(entity_id):
@@ -39,18 +43,36 @@ def get_types(entity_id):
     :return: <list> of entity, each is a type of the target entity
     """
     dbpedia_sql = """
-PREFIX dbo: <http://dbpedia.org/ontology>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-SELECT DISTINCT ?type
-WHERE {
-   {<%s> dbo:type ?type .}
-   UNION
-   {<%s> rdf:type ?type .}
-}
-ORDER BY ?type
-    """% (entity_id, entity_id)
-    results = __execute_sparql(DBPEDIA_ENDPOINT, dbpedia_sql)['results']['bindings']
-    return [result['type']['value'] for result in results]
+        PREFIX dbo: <http://dbpedia.org/ontology>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX dbr: <http://dbpedia.org/resource>
+        SELECT DISTINCT ?type
+        WHERE {
+           {<%s> dbo:type ?type .}
+           UNION
+           {<%s> rdf:type ?type .}
+           UNION
+           {<%s> owl:type ?type .}
+           UNION
+           {<%s> dbr:type ?type .}
+        }
+        ORDER BY ?type
+    """ % (entity_id,entity_id,entity_id,entity_id)
+    results = __execute_sparql(DBPEDIA_ENDPOINT, dbpedia_sql)["results"]["bindings"]
+    return [result["type"]["value"] for result in results]
+
+def get_subjects(entity_id):
+    dbpedia_sql = """
+        PREFIX dct:<http://purl.org/dc/terms/>
+        SELECT DISTINCT ?subject
+        WHERE {
+           <%s> dct:subject ?subject .
+        }
+        ORDER BY ?subject
+    """ % entity_id
+    results = __execute_sparql(DBPEDIA_ENDPOINT, dbpedia_sql)["results"]["bindings"]
+    return [result["subject"]["value"] for result in results]
 
 def get_pv_pairs(entity_id):
     """
@@ -59,14 +81,15 @@ def get_pv_pairs(entity_id):
     :return: <list> of (property, value)
     """
     dbpedia_sql = """
-SELECT DISTINCT ?p ?o
-WHERE {
-    <%s> ?p ?o
-}
-ORDER BY ?p
-    """% (entity_id)
-    results = __execute_sparql(DBPEDIA_ENDPOINT, dbpedia_sql)['results']['bindings']
-    return [(result['p']['value'], result['o']['value']) for result in results]
+        SELECT DISTINCT ?p ?o
+        WHERE {
+            <%s> ?p ?o
+        }
+        ORDER BY ?p
+    """ % entity_id
+    results = __execute_sparql(DBPEDIA_ENDPOINT, dbpedia_sql)["results"]["bindings"]
+    return [(result["p"]["value"], result["o"]["value"]) for result in results]
+
 
 def get_csks(entity_id):
     """
@@ -75,32 +98,35 @@ def get_csks(entity_id):
     :return: <list> of (property, value)
     """
     # TODO
-    pass
+    return []
+
 
 def get_all_subjects(property_id):
     dbpedia_sql = """
-SELECT ?s
-WHERE{
-    ?s <%s> ?o .
-}
-    """%(property_id)
-    results = __execute_sparql(DBPEDIA_ENDPOINT, dbpedia_sql)['results']['bindings']
-    return [ result['s']['value'] for result in results ]
+        SELECT ?s
+        WHERE{
+            ?s <%s> ?o .
+        }
+    """ % property_id
+    results = __execute_sparql(DBPEDIA_ENDPOINT, dbpedia_sql)["results"]["bindings"]
+    return [result["s"]["value"] for result in results]
+
 
 def get_all_type_member(type_id):
     dbpedia_sql = """
-    PREFIX dbo: <http://dbpedia.org/ontology>
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    SELECT DISTINCT ?subject
-    WHERE {
-       {?subject dbo:type <%s> .}
-       UNION
-       {?subject rdf:type <%s> .}
-    }
-    ORDER BY ?type
-        """ % (type_id, type_id)
-    results = __execute_sparql(DBPEDIA_ENDPOINT, dbpedia_sql)['results']['bindings']
-    return [result['subject']['value'] for result in results]
+        PREFIX dbo: <http://dbpedia.org/ontology>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        SELECT DISTINCT ?subject
+        WHERE {
+           {?subject dbo:type <%s> .}
+           UNION
+           {?subject rdf:type <%s> .}
+        }
+        ORDER BY ?type
+    """ % (type_id, type_id)
+    results = __execute_sparql(DBPEDIA_ENDPOINT, dbpedia_sql)["results"]["bindings"]
+    return [result["subject"]["value"] for result in results]
+
 
 def get_resource_name(res_id):
     dbpedia_sql = """
@@ -112,24 +138,35 @@ def get_resource_name(res_id):
             {<%s> foaf:name ?name . }
             UNION
             {<%s> rdfs:label ?name . }
-        }""" % (res_id, res_id)
-    results = __execute_sparql(dbpedia_sql)
-    return [result['name']['value'] for result in results]
+        }
+    """ % (res_id, res_id)
+    results = __execute_sparql(DBPEDIA_ENDPOINT, dbpedia_sql)["results"]["bindings"]
+    return [result["name"]["value"] for result in results]
+
 
 def get_parent_class(class_id):
     dbpedia_sql = """
-            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema>
-            SELECT DISTINCT ?o
-            FROM NAMED <http://dbpedia.org/resource/classes#>
-            WHERE {
-                GRAPH <http://dbpedia.org/resource/classes#>{
-                    <%s> rdfs:subClassOf ?o
-                }
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        SELECT DISTINCT ?o
+        FROM NAMED <http://dbpedia.org/resource/classes#>
+        WHERE {
+            GRAPH <http://dbpedia.org/resource/classes#>{
+                <%s> rdfs:subClassOf ?o
             }
-    """%(class_id)
+        }
+    """ % class_id
+    results = __execute_sparql(DBPEDIA_ENDPOINT, dbpedia_sql)["results"]["bindings"]
+    return [result["o"]["value"] for result in results]
 
-    results = __execute_sparql(dbpedia_sql)['results']['bindings']
-    return [result['o']['value'] for result in results]
+
+def filter_get_ontology(all_types):
+    s=set()
+    for x in all_types:
+        pattern = re.compile('http://dbpedia.org/ontology/.+')
+        match = pattern.match(x)
+        if not match:
+            s.add(x)
+    return all_types-s
 
 def __execute_sparql(endpoint, sql):
     sparql = SPARQLWrapper(endpoint)
@@ -137,19 +174,28 @@ def __execute_sparql(endpoint, sql):
     sparql.setReturnFormat(JSON)
     return sparql.query().convert()
 
-if __name__ == '__main__':
-    #print "---category---"
-    #for cate in get_categories('http://dbpedia.org/resource/Eiffel_Tower'):
+if __name__ == "__main__":
+    logging.basicConfig(format="%(asctime)s: %(levelname)s: %(message)s")
+    logging.root.setLevel(level=logging.INFO)
+
+    all_subjects=get_subjects("http://dbpedia.org/resource/Tiger")
+    print all_subjects
+
+    superclass=get_parent_class("http://dbpedia.org/ontology/Town")[0]
+    print superclass
+
+    # print "---category---"
+    # for cate in get_categories("http://dbpedia.org/resource/Eiffel_Tower"):
     #    print cate
-    #print "---type---"
-    #for type in get_types('http://dbpedia.org/resource/Eiffel_Tower'):
+    # print "---type---"
+    # for type in get_types("http://dbpedia.org/resource/Eiffel_Tower"):
     #    print type
-    #print "---pv pairs---"
-    #for pair in get_pv_pairs('http://dbpedia.org/resource/Eiffel_Tower'):
+    # print "---pv pairs---"
+    # for pair in get_pv_pairs("http://dbpedia.org/resource/Eiffel_Tower"):
     #    print pair
-    #print "---'abstract'_is_multi_value---"
-    #pairs = get_all_subjects(u'http://dbpedia.org/ontology/abstract')
-    #print len(pairs) > len(set(pairs))
-    print "---Arch structure type member---"
-    for m in get_all_type_member("http://dbpedia.org/ontology/ArchitecturalStructure"):
-        print m
+    # print "---"abstract"_is_multi_value---"
+    # pairs = get_all_subjects(u"http://dbpedia.org/ontology/abstract")
+    # print len(pairs) > len(set(pairs))
+    # print("---Arch structure type member---")
+    # for m in get_all_type_member("http://dbpedia.org/ontology/ArchitecturalStructure"):
+    #     print(m)
--- extraction.py
+++ extraction.py
@@ -4,66 +4,87 @@
 """
     
 """
+import logging
 from collections import Counter
 
-from . import dataset
+import dataset
 
 import numpy
+
 __author__ = "freemso"
 
+# Hyper-parameters
+ALPHA = 0.7
 
-class Extractor(object):
+id_node_map = {}
+
+
+def id2node(uuid):
     """
-    Common sense knowledge extractor
+    Get the node of id
+    :param uuid: universal identifier of the node
+    :return: <Node>
     """
-    def __init__(self, alpha):
-        self.alpha = alpha
-        self.id2node = {}
-
-    def get_node(self, uuid):
-        """
-        Get the node of id
-        :param uuid: universal identifier of the node
-        :return: <Node>
-        """
-        if uuid in self.id2node:
-            return self.id2node[uuid]
-        else:
-            node = Node(uuid)
-            self.id2node[uuid] = node
-            return node
-
-    def extract(self, target_uuid):
-        target_node = self.get_node(target_uuid)
-        parents = target_node.get_parents()
-        siblings = target_node.get_siblings()
-        target_attributes = target_node.get_attributes()
-        num_parent = len(parents)
-        num_sibling = len(siblings)
-        parents_attributes_counter = self.count_nodes_attributes(parents)
-        siblings_attributes_counter = self.count_nodes_attributes(siblings)
-
-        # Inference from sibling
-        for attribute in siblings_attributes_counter:
-            if siblings_attributes_counter[attribute] < num_sibling ** self.alpha:
-                pass
-                # TODO
+    if uuid in id_node_map:
+        return id_node_map[uuid]
+    else:
+        node = Node(uuid)
+        id_node_map[uuid] = node
+        return node
 
 
+def extract(target_uuid):
+    target_node = id2node(target_uuid)
+    logging.info("Extracting common sense knowledge for {}".format(target_node.get_name()))
 
-    @staticmethod
-    def count_nodes_attributes(nodes):
-        """
-        Count the attributes of a list of nodes
-        :param nodes: <list> of <Node>
-        :return: <Counter> of attributes with their occurrences
-        """
-        attributes_counter = Counter()
-        for node in nodes:
-            attributes = node.get_attributes()
-            for attribute in attributes:
-                attributes_counter[attribute] += 1
-        return attributes_counter
+    parents = target_node.get_parents()
+    num_parents = len(parents)
+    logging.info("{} has {} parents: {}".format(
+        target_node.get_name(),
+        num_parents,
+        ", ".join([p.get_name() for p in parents])
+    ))
+
+    siblings = target_node.get_siblings()
+    num_siblings = len(siblings)
+    logging.info("{} has {} siblings: {}".format(
+        target_node.get_name(),
+        num_siblings,
+        ", ".join([s.get_name() for s in siblings])
+    ))
+
+    target_attributes = target_node.get_attributes()
+
+    parents_attributes_counter = count_nodes_attributes(parents)
+    siblings_attributes_counter = count_nodes_attributes(siblings)
+
+    # Inference from sibling
+    for attribute in siblings_attributes_counter:
+        if siblings_attributes_counter[attribute] > num_siblings ** ALPHA:
+            # This attribute is valid
+            pass
+
+    # Inherit from parent
+    # TODO
+
+    # Merge results(from parents and from siblings) and conflict detection
+    # TODO
+
+    # Show the result
+
+
+def count_nodes_attributes(nodes):
+    """
+    Count the attributes of a list of nodes
+    :param nodes: <list> of <Node>
+    :return: <Counter> of attributes with their occurrences
+    """
+    attributes_counter = Counter()
+    for node in nodes:
+        attributes = node.get_attributes()
+        for attribute in attributes:
+            attributes_counter[attribute] += 1
+    return attributes_counter
 
 
 class Node(object):
@@ -71,11 +92,12 @@ class Node(object):
     Node of the knowledge graph.
     Entity, category, type and value in PV-pair are all nodes.
     """
+
     def __init__(self, uuid):
         """
         :param uuid: universal identifier
         """
-        self.uuid = uuid # uri
+        self.uuid = uuid  # uri
         self.parents = None
         self.siblings = None
         self.attributes = None  # CSK from ConceptNet and PV-pairs from DB-pedia are all considered as attributes
@@ -94,19 +116,32 @@ class Node(object):
         :return: <str>
         """
         names = dataset.get_resource_name(self.uuid)
-        return names[0] if len(names)>0 else self.uuid
+        return names[0] if len(names) > 0 else self.uuid.rsplit("/", 1)[-1]
 
     def get_parents(self):
         """
         Get parent nodes of the target node
         :return: <list> of <Node>
         """
+        self.parents = set()
         if not self.parents:
-            all = [Node(id) for id in dataset.get_types(self.uuid)]
-            self.parents = all[:]
-            for p in all:
-                subClassOf = dataset.get_parent_class(p.uuid)
-                self.parents = [p for p in self.parents if p.uuid not in subClassOf]
+            all_subjects={subject_id for subject_id in dataset.get_subjects(self.uuid)}
+            all_types = {type_id for type_id in dataset.get_types(self.uuid)}
+            ontology_types=dataset.filter_get_ontology(all_types)
+            for type_id in ontology_types:
+                p=type_id
+                while True :
+                    temp = dataset.get_parent_class(p)
+                    if len(temp)==0:
+                        break
+                    else:
+                        p=temp[0]
+                        if p in all_types:
+                            all_types.remove(p)
+                        else:
+                            continue
+            all_parents=all_types|all_subjects
+            self.parents = [id2node(parent_id) for parent_id in all_parents]
         return self.parents
 
     def get_siblings(self):
@@ -115,10 +150,10 @@ class Node(object):
         :return: <list> of <Node>
         """
         if not self.siblings:
-            self.siblings = []
+            self.siblings = set()
             for p in self.parents:
                 children = dataset.get_all_type_member(p.uuid)
-                self.siblings.append([Node(c) for c in children])
+                self.siblings = self.siblings.union({id2node(c) for c in children})
         return self.siblings
 
     def get_attributes(self):
@@ -131,54 +166,47 @@ class Node(object):
             pv_pairs = dataset.get_pv_pairs(self.uuid)
             csks = dataset.get_csks(self.uuid)
             # Merge duplicated attributes
-            self.attributes = list(set(pv_pairs + csks))
+            self.attributes = set(pv_pairs + csks)
 
         if self.extracted_correct_attributes:
-            return self.attributes + self.extracted_correct_attributes
+            return self.attributes.union(self.extracted_correct_attributes)
         else:
             return self.attributes
 
 
-class Edge(object):
+def is_multi_valued(property_id):
     """
-    Edge of the knowledge graph.
-    Property in PV-pair and relation in CSK are both edges.
+    Check if the edge is multi-valued.
+    Calculate all predicates of <SPO> in DB-pedia, those objectives values of
+    predicates that occur more than once are treated as multi-value attributes.
+    :return: <bool>
     """
-    def __init__(self, uuid, name):
-        """
-        :param uuid: universal identifier
-        :param name: human readable name
-        """
-        self.uuid = uuid
-        self.name = name
+    pairs = dataset.get_all_subjects(property_id)
+    return len(pairs) > len(set(pairs))
 
-    def __hash__(self):
-        return hash(self.uuid)
 
-    def is_multi_valued(self):
-        """
-        Check if the edge is multi-valued.
-        Calculate all predicates of <SPO> in DB-pedia, those objectives values of
-        predicates that occur more than once are treated as multi-value attributes.
-        :return: <bool>
-        """
-        pairs = dataset.get_all_subjects(self.uuid)
-        return len(pairs) > len(set(pairs))
+# def immediate_category_filter(category_nodes):
+#     """
+#     Filter out those categories who are at a higher level in the hierarchy.
+#     Hierarchy information is available in DB-pedia Ontology.
+#     :param category_nodes: <list> of <Node>
+#     :return: <list> of <Node>, with no hierarchy conflict(granularity)
+#     """
+#     origin = category_nodes[:]
+#     result = category_nodes[:]
+#
+#     for id in origin:
+#         cur_cate = dataset.get_categories(id)  # Does Category has parent category?
+#         result = list(set(result) - (set(result) & set(cur_cate)))
+#     return result
 
-def immediate_category_filter(category_nodes):
-    """
-    Filter out those categories who are at a higher level in the hierarchy.
-    Hierarchy information is available in DB-pedia Ontology.
-    :param category_nodes: <list> of <Node>
-    :return: <list> of <Node>, with no hierarchy conflict(granularity)
-    """
-    origin = category_nodes[:]
-    result = category_nodes[:]
-
-    for id in origin:
-        cur_cate = dataset.get_categories(id) # Does Category has parent category?
-        result = list( set(result) - (set(result) & set(cur_cate)) )
-    return result
 
 if __name__ == '__main__':
-    pass
+    logging.basicConfig(format="%(asctime)s: %(levelname)s: %(message)s")
+    logging.root.setLevel(level=logging.INFO)
+    node = id2node("http://dbpedia.org/resource/Huawei_P9")
+    all_parents=node.get_parents()
+    for x in all_parents:
+        print x.uuid
+
+
