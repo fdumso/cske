ee686816e8933358fff553e318b3c33648cc04b4
Sephirothxlx <1216083287@qq.com>
2017-08-17 17:41:44 +0800

    revise all the functions
    divide the whole function into three different parts
    google baidu bing

--- search.py
+++ search.py
@@ -1,22 +1,45 @@
+#coding:utf-8
+
 import requests
+import re
 from bs4 import BeautifulSoup
 
-#return "About 1,000,000 results"
-def get_search_results_text(keyword):
-	r = requests.get("https://www.google.com/search",params={'q':keyword})
-	soup = BeautifulSoup(r.text,"lxml")
-	res = soup.find("div",{"id":"resultStats"})
-	return res.text
+#all the following functions return 1000000, which is a integer
 
-#return 1000000, that is a integer
-def get_search_results_number(keyword):
+def get_search_results_Google(keyword):
 	r = requests.get("https://www.google.com/search",params={'q':keyword})
-	soup = BeautifulSoup(r.text,"lxml")
+	soup = BeautifulSoup(r.text)
 	res = soup.find("div",{"id":"resultStats"})
-	n_text = res.split(' ')[1].split(',')
+	n_text = res.text.split(' ')[1].split(',')
 	number = ""
 	for n in n_text:
 		number+=n
 	return int(number)
 
-print get_search_results_number("android device")
+def get_search_results_Baidu(keyword):
+	r = requests.get("http://www.baidu.com/s",params={'wd':keyword})
+	soup = BeautifulSoup(r.text)
+	res = soup.find("div",{"class":"nums"})
+	n_text=re.findall(r"\d+",res.text)
+	number=""
+	for n in n_text:
+		number+=n
+	return int(number)
+
+def get_search_results_Bing(keyword):
+	r = requests.get("https://www.bing.com/search",params={'q':keyword})
+	soup = BeautifulSoup(r.text)
+	res = soup.find("span",{"class":"sb_count"})
+	n_text = res.text.split(' ')[0].split(',')
+	number = ""
+	for n in n_text:
+		number+=n
+	return int(number)
+
+#print get_search_results_Google("test")
+print get_search_results_Baidu("huawei_p9 android")
+print get_search_results_Baidu("huawei_p9 ios")
+print get_search_results_Bing("huawei_p9 android")
+print get_search_results_Bing("huawei_p9 ios")
+
+
